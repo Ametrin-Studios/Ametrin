1786104126871507d9eea26c62a9d92ae56097a8
Barion <kothe@botos.de>
2022-09-09 18:38:42 +0200

    added growableBushBlock() model provider

--- src/main/java/com/ametrinstudios/ametrin/datagen/ExtendedBlockStateProvider.java
+++ src/main/java/com/ametrinstudios/ametrin/datagen/ExtendedBlockStateProvider.java
@@ -1,8 +1,6 @@
 package com.ametrinstudios.ametrin.datagen;
 
-import com.ametrinstudios.ametrin.world.block.AmAbstractHeadBlock;
-import com.ametrinstudios.ametrin.world.block.CustomHeadBlock;
-import com.ametrinstudios.ametrin.world.block.CustomWallHeadBlock;
+import com.ametrinstudios.ametrin.world.block.*;
 import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
@@ -43,6 +41,7 @@ public abstract class ExtendedBlockStateProvider extends BlockStateProvider {
         excludedClasses.add(SignBlock.class); //may be automated in the future;
         excludedClasses.add(BaseFireBlock.class); //may be automated in the future;
         excludedClasses.add(LiquidBlock.class); //may be automated in the future;
+        excludedClasses.add(GrowableDoublePlantBlock.class);
     }
 
     public ExtendedBlockStateProvider(DataGenerator generator, String modID, ExistingFileHelper existingFileHelper) {
@@ -151,6 +150,8 @@ public abstract class ExtendedBlockStateProvider extends BlockStateProvider {
                 ModelFile top = models().withExistingParent(name + "_top", "block/tinted_cross").texture("cross", modBlockLoc(texture + "_top")).renderType(RenderTypes.Cutout);
                 ModelFile bottom = models().withExistingParent(name + "_bottom", "block/tinted_cross").texture("cross", modBlockLoc(texture + "_bottom")).renderType(RenderTypes.Cutout);
                 getVariantBuilder(block).forAllStates(state -> ConfiguredModel.builder().modelFile(state.getValue(DoublePlantBlock.HALF) == DoubleBlockHalf.LOWER ? bottom : top).build());
+            }else if(block instanceof GrowableBushBlock){
+                simpleGrowableBushBlock((GrowableBushBlock) block, name, texture);
             }else if(block instanceof BushBlock || block instanceof GrowingPlantBlock){
                 simpleBlock(block, models().withExistingParent(name, "block/tinted_cross").texture("cross", modBlockLoc(name)).renderType(RenderTypes.Cutout));
             }else if(block instanceof FlowerPotBlock){
@@ -221,6 +222,36 @@ public abstract class ExtendedBlockStateProvider extends BlockStateProvider {
         getVariantBuilder(block).forAllStatesExcept(state -> ConfiguredModel.builder().modelFile(model).rotationY(horizontalDirectionToYAngle(state.getValue(CustomWallHeadBlock.Facing))).build(), AmAbstractHeadBlock.Waterlogged);
     }
 
+    protected void simpleGrowableBushBlock(GrowableBushBlock bush, String name, String texture) {
+        ModelFile Age0 = models().cross("block/" + name + "/stage0", mcLoc("block/sweet_berry_bush_stage0")).renderType(RenderTypes.Cutout);
+        ModelFile Age1 = models().cross("block/" + name + "/stage1", mcLoc("block/sweet_berry_bush_stage1")).renderType(RenderTypes.Cutout);
+        ModelFile Age2 = models().cross("block/" + name + "/stage2", modBlockLoc(texture + "/stage2")).renderType(RenderTypes.Cutout);
+        ModelFile Age3 = models().cross("block/" + name + "/stage3", modBlockLoc(texture + "/stage3")).renderType(RenderTypes.Cutout);
+        getVariantBuilder(bush).forAllStates(state -> {
+            final int age = state.getValue(IGrowable.Age);
+            return ConfiguredModel.builder().modelFile((age == 0) ? Age0 : (age == 1) ? Age1 : (age == 2) ? Age2 : Age3).build();
+        });
+    }
+    protected void growableBushBlock(GrowableBushBlock bush, String name, String texture) {
+        ModelFile Age0 = models().cross("block/" + name + "/stage0", modBlockLoc(texture + "/stage0")).renderType(RenderTypes.Cutout);
+        ModelFile Age1 = models().cross("block/" + name + "/stage1", modBlockLoc(texture + "/stage1")).renderType(RenderTypes.Cutout);
+        ModelFile Age2 = models().cross("block/" + name + "/stage2", modBlockLoc(texture + "/stage2")).renderType(RenderTypes.Cutout);
+        ModelFile Age3 = models().cross("block/" + name + "/stage3", modBlockLoc(texture + "/stage3")).renderType(RenderTypes.Cutout);
+        getVariantBuilder(bush).forAllStates(state -> {
+            final int age = state.getValue(IGrowable.Age);
+            return ConfiguredModel.builder().modelFile((age == 0) ? Age0 : (age == 1) ? Age1 : (age == 2) ? Age2 : Age3).build();
+        });
+    }
+
+    /**
+     * converts a horizontal direction to the corresponding Y angle
+     * @param direction horizontal direction
+     * @return y angle of the direction
+     */
+    protected static int horizontalDirectionToYAngle(Direction direction){
+        return direction == Direction.NORTH ? 0 : direction == Direction.EAST ? 90 : direction == Direction.SOUTH ? 180 : 270;
+    }
+
     /**
      * converts a horizontal direction to a wall torch specific Y angle use {@link #horizontalDirectionToYAngle(Direction)} for a standardised method
      * @param direction horizontal direction
@@ -236,14 +267,6 @@ public abstract class ExtendedBlockStateProvider extends BlockStateProvider {
      */
     protected static int horizontalDirectionToYAngleForCampfire(Direction direction){
         return direction == Direction.NORTH ? 270 : direction == Direction.EAST ? 0 : direction == Direction.SOUTH ? 90 : 180;
-    }
-    /**
-     * converts a horizontal direction to the corresponding Y angle
-     * @param direction horizontal direction
-     * @return y angle of the direction
-     */
-    protected static int horizontalDirectionToYAngle(Direction direction){
-        return direction == Direction.NORTH ? 0 : direction == Direction.EAST ? 90 : direction == Direction.SOUTH ? 180 : 270;
     }
 
     protected ResourceLocation modBlockLoc(String key) {return modLoc("block/" + key);}
