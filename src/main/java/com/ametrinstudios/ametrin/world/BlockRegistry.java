a7bcde8d2e227666a463d99a90106ffd5065b696
Barion <kothe@botos.de>
2022-10-09 20:17:17 +0200

    refactoring

--- src/main/java/com/ametrinstudios/ametrin/world/BlockRegistry.java
+++ src/main/java/com/ametrinstudios/ametrin/world/BlockRegistry.java
@@ -14,36 +14,36 @@ import java.util.function.Supplier;
 import java.util.function.ToIntFunction;
 
 public abstract class BlockRegistry {
-    protected static BlockBehaviour.Properties properties(Block base) {return BlockBehaviour.Properties.copy(base);}
+    protected static BlockBehaviour.Properties properties(Block parent) {return BlockBehaviour.Properties.copy(parent);}
     protected static BlockBehaviour.Properties properties(Material material) {return BlockBehaviour.Properties.of(material);}
 
     protected static Supplier<Block> block(BlockBehaviour.Properties properties) {return ()-> new Block(properties);}
-    protected static Supplier<Block> block(Block base) {return block(properties(base));}
+    protected static Supplier<Block> block(Block parent) {return block(properties(parent));}
     protected static Supplier<DropExperienceBlock> xpDroppingBlock(UniformInt xp, BlockBehaviour.Properties properties) {return ()-> new DropExperienceBlock(properties, xp);}
     protected static Supplier<StairBlock> stair(StairBlock.Properties properties, Supplier<BlockState> base) {return ()-> new StairBlock(base, properties);}
-    protected static Supplier<StairBlock> stair(Block base) {return stair(properties(base), base::defaultBlockState);}
+    protected static Supplier<StairBlock> stair(Block parent) {return stair(properties(parent), parent::defaultBlockState);}
     protected static Supplier<SlabBlock> slab(SlabBlock.Properties properties) {return ()-> new SlabBlock(properties);}
-    protected static Supplier<SlabBlock> slab(Block base) {return slab(properties(base));}
+    protected static Supplier<SlabBlock> slab(Block parent) {return slab(properties(parent));}
     protected static Supplier<WallBlock> wall(WallBlock.Properties properties) {return ()-> new WallBlock(properties);}
-    protected static Supplier<WallBlock> wall(Block base) {return wall(properties(base));}
+    protected static Supplier<WallBlock> wall(Block parent) {return wall(properties(parent));}
     protected static Supplier<FenceBlock> fence(FenceBlock.Properties properties) {return ()-> new FenceBlock(properties);}
-    protected static Supplier<FenceBlock> fence(Block base) {return fence(properties(base));}
+    protected static Supplier<FenceBlock> fence(Block parent) {return fence(properties(parent));}
     protected static Supplier<FenceGateBlock> fenceGate(FenceGateBlock.Properties properties) {return ()-> new FenceGateBlock(properties);}
-    protected static Supplier<FenceGateBlock> fenceGate(Block base) {return fenceGate(properties(base));}
+    protected static Supplier<FenceGateBlock> fenceGate(Block parent) {return fenceGate(properties(parent));}
     protected static Supplier<PressurePlateBlock> pressurePlate(PressurePlateBlock.Sensitivity sensitivity, BlockBehaviour.Properties properties) {return ()-> new PressurePlateBlock(sensitivity, properties);}
-    protected static Supplier<PressurePlateBlock> pressurePlate(PressurePlateBlock.Sensitivity sensitivity, Block base) {return (pressurePlate(sensitivity, properties(base)));}
+    protected static Supplier<PressurePlateBlock> pressurePlate(PressurePlateBlock.Sensitivity sensitivity, Block parent) {return (pressurePlate(sensitivity, properties(parent)));}
     protected static Supplier<WoodButtonBlock> woodButton(BlockBehaviour.Properties properties) {return ()-> new WoodButtonBlock(properties);}
-    protected static Supplier<WoodButtonBlock> woodButton(Block base) {return woodButton(properties(base));}
+    protected static Supplier<WoodButtonBlock> woodButton(Block parent) {return woodButton(properties(parent));}
     protected static Supplier<LeavesBlock> leave(BlockBehaviour.Properties properties) {return ()-> new LeavesBlock(properties);}
-    protected static Supplier<LeavesBlock> leave(Block base) {return leave(properties(base));}
+    protected static Supplier<LeavesBlock> leave(Block parent) {return leave(properties(parent));}
     protected static Supplier<SaplingBlock> sapling(Supplier<? extends CustomTreeFeature> tree) {return ()-> new SaplingBlock(new CustomTreeGrower(tree), properties(Blocks.OAK_SAPLING));}
     protected static Supplier<FlowerPotBlock> potted(Supplier<Block> main) {return ()-> new FlowerPotBlock(() -> (FlowerPotBlock)Blocks.FLOWER_POT, main, properties(Blocks.POTTED_OAK_SAPLING));}
     protected static Supplier<DoorBlock> door(BlockBehaviour.Properties properties) {return ()-> new DoorBlock(properties);}
-    protected static Supplier<DoorBlock> door(Block base) {return door(properties(base));}
+    protected static Supplier<DoorBlock> door(Block parent) {return door(properties(parent));}
     protected static Supplier<TrapDoorBlock> trapdoor(BlockBehaviour.Properties properties) {return ()-> new TrapDoorBlock(properties);}
-    protected static Supplier<TrapDoorBlock> trapdoor(Block base) {return trapdoor(properties(base));}
+    protected static Supplier<TrapDoorBlock> trapdoor(Block parent) {return trapdoor(properties(parent));}
     protected static Supplier<RotatedPillarBlock> rotatedPillar(BlockBehaviour.Properties properties) {return ()-> new RotatedPillarBlock(properties);}
-    protected static Supplier<RotatedPillarBlock> rotatedPillar(Block base) {return rotatedPillar(properties(base));}
+    protected static Supplier<RotatedPillarBlock> rotatedPillar(Block parent) {return rotatedPillar(properties(parent));}
     protected static Supplier<AgeableBushBlock> bush(int bonusDrop, int growRarity) {return ()-> new AgeableBushBlock(bonusDrop, growRarity, properties(Blocks.SWEET_BERRY_BUSH));}
 
     protected static ToIntFunction<BlockState> litEmission(int lightLevel) {return (state)-> state.getValue(BlockStateProperties.LIT) ? lightLevel : 0;}
