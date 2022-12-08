cd21a8680cca434e256ff377d4b78404bc42243d
Barion <kothe@botos.de>
2022-12-08 17:01:12 +0100

    added helper methods for buttons to `BlockRegistry` removed deprecated methods

--- src/main/java/com/ametrinstudios/ametrin/world/BlockRegistry.java
+++ src/main/java/com/ametrinstudios/ametrin/world/BlockRegistry.java
@@ -1,10 +1,12 @@
 package com.ametrinstudios.ametrin.world;
 
 import com.ametrinstudios.ametrin.util.mixin.IMixinBlockBehaviorProperties;
-import com.ametrinstudios.ametrin.world.block.*;
+import com.ametrinstudios.ametrin.world.block.AgeableBushBlock;
+import com.ametrinstudios.ametrin.world.block.FlammableStairPlankBlock;
 import com.ametrinstudios.ametrin.world.gen.feature.tree.CustomTreeFeature;
 import com.ametrinstudios.ametrin.world.gen.feature.tree.CustomTreeGrower;
-import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,47 +16,26 @@ import net.minecraft.world.level.material.Material;
 import java.util.function.Supplier;
 import java.util.function.ToIntFunction;
 
+@SuppressWarnings("unused")
 public abstract class BlockRegistry {
     protected static BlockBehaviour.Properties properties(Block parent) {return BlockBehaviour.Properties.copy(parent);}
     protected static BlockBehaviour.Properties properties(Material material) {return BlockBehaviour.Properties.of(material);}
     protected static BlockBehaviour.Properties properties(BlockBehaviour.Properties properties) {return ((IMixinBlockBehaviorProperties) properties).copy();}
 
-    @Deprecated(forRemoval = true) protected static Supplier<Block> block(BlockBehaviour.Properties properties) {return ()-> new Block(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<Block> block(Block parent) {return block(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<DropExperienceBlock> xpDroppingBlock(IntProvider xp, BlockBehaviour.Properties properties) {return ()-> new DropExperienceBlock(properties, xp);}
     protected static Supplier<StairBlock> stair(StairBlock.Properties properties, Supplier<BlockState> base) {return ()-> new StairBlock(base, properties);}
     protected static Supplier<StairBlock> stair(Block parent) {return stair(properties(parent), parent::defaultBlockState);}
     protected static Supplier<FlammableStairPlankBlock> flammableStair(FlammableStairPlankBlock.Properties properties, Supplier<BlockState> base) {return ()-> new FlammableStairPlankBlock(base, properties);}
     protected static Supplier<FlammableStairPlankBlock> flammableStair(Block parent) {return flammableStair(properties(parent), parent::defaultBlockState);}
-    @Deprecated(forRemoval = true) protected static Supplier<SlabBlock> slab(SlabBlock.Properties properties) {return ()-> new SlabBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<SlabBlock> slab(Block parent) {return slab(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<WallBlock> wall(WallBlock.Properties properties) {return ()-> new WallBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<WallBlock> wall(Block parent) {return wall(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<FenceBlock> fence(FenceBlock.Properties properties) {return ()-> new FenceBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<FenceBlock> fence(Block parent) {return fence(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<FenceGateBlock> fenceGate(FenceGateBlock.Properties properties) {return ()-> new FenceGateBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<FenceGateBlock> fenceGate(Block parent) {return fenceGate(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<PressurePlateBlock> pressurePlate(PressurePlateBlock.Sensitivity sensitivity, BlockBehaviour.Properties properties) {return ()-> new PressurePlateBlock(sensitivity, properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<PressurePlateBlock> pressurePlate(PressurePlateBlock.Sensitivity sensitivity, Block parent) {return (pressurePlate(sensitivity, properties(parent)));}
-    @Deprecated(forRemoval = true) protected static Supplier<WoodButtonBlock> woodButton(BlockBehaviour.Properties properties) {return ()-> new WoodButtonBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<WoodButtonBlock> woodButton(Block parent) {return woodButton(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<StoneButtonBlock> stoneButton(BlockBehaviour.Properties properties) {return ()-> new StoneButtonBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<StoneButtonBlock> stoneButton(Block parent) {return stoneButton(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammableLeavesBlock> leave(BlockBehaviour.Properties properties) {return ()-> new FlammableLeavesBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammableLeavesBlock> leave(Block parent) {return leave(properties(parent));}
     protected static Supplier<SaplingBlock> sapling(Supplier<? extends CustomTreeFeature> tree) {return ()-> new SaplingBlock(new CustomTreeGrower(tree), properties(Blocks.OAK_SAPLING));}
     protected static Supplier<FlowerPotBlock> potted(Supplier<Block> main) {return ()-> new FlowerPotBlock(() -> (FlowerPotBlock)Blocks.FLOWER_POT, main, properties(Blocks.POTTED_OAK_SAPLING));}
-    @Deprecated(forRemoval = true) protected static Supplier<DoorBlock> door(BlockBehaviour.Properties properties) {return ()-> new DoorBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<DoorBlock> door(Block parent) {return door(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<TrapDoorBlock> trapdoor(BlockBehaviour.Properties properties) {return ()-> new TrapDoorBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<TrapDoorBlock> trapdoor(Block parent) {return trapdoor(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<RotatedPillarBlock> rotatedPillar(BlockBehaviour.Properties properties) {return ()-> new RotatedPillarBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<RotatedPillarBlock> rotatedPillar(Block parent) {return rotatedPillar(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammablePlankBlock> plank(BlockBehaviour.Properties properties) {return ()-> new FlammablePlankBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammablePlankBlock> plank(Block parent) {return plank(properties(parent));}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammableLogBlock> flammableLog(BlockBehaviour.Properties properties) {return ()-> new FlammableLogBlock(properties);}
-    @Deprecated(forRemoval = true) protected static Supplier<FlammableLogBlock> flammableLog(Block parent) {return flammableLog(properties(parent));}
     protected static Supplier<AgeableBushBlock> bush(int bonusDrop, int growRarity) {return ()-> new AgeableBushBlock(bonusDrop, growRarity, properties(Blocks.SWEET_BERRY_BUSH));}
+
+    protected static Supplier<ButtonBlock> woodenButton() {return woodenButton(SoundType.WOOD, SoundEvents.WOODEN_BUTTON_CLICK_OFF, SoundEvents.WOODEN_BUTTON_CLICK_ON);}
+    protected static Supplier<ButtonBlock> woodenButton(SoundType soundType, SoundEvent soundOffEvent, SoundEvent soundOnEvent) {return button(30, true, soundType, soundOffEvent, soundOnEvent);}
+    protected static Supplier<ButtonBlock> stoneButton() {return button(20, false, SoundType.STONE, SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON);}
+    protected static Supplier<ButtonBlock> button(int ticksStayPressed, boolean arrowsCanPress, SoundType soundType, SoundEvent soundOffEvent, SoundEvent soundOnEvent){
+        return ()-> new ButtonBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().strength(0.5F).sound(soundType), ticksStayPressed, arrowsCanPress, soundOffEvent, soundOnEvent);
+    }
 
     protected static ToIntFunction<BlockState> litEmission(int lightLevel) {return (state)-> state.getValue(BlockStateProperties.LIT) ? lightLevel : 0;}
     protected static ToIntFunction<BlockState> litEmission(ToIntFunction<BlockState> lightLevel) {return (state)-> state.getValue(BlockStateProperties.LIT) ? lightLevel.applyAsInt(state) : 0;}
