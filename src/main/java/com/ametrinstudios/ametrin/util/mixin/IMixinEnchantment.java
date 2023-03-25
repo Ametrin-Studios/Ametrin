71bfdddfc50434a722ae5acef0c3f58b9ab5d165
Barion <kothe@botos.de>
2023-03-25 11:28:03 +0100

    removed MixinEnchantment as it is obsolete and doesn't work

--- src/main/java/com/ametrinstudios/ametrin/util/mixin/IMixinEnchantment.java
+++
@@ -1,9 +0,0 @@
-package com.ametrinstudios.ametrin.util.mixin;
-
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
-
-public interface IMixinEnchantment {
-    EquipmentSlot[] getApplicableSlots();
-    boolean hasEnchantment(LivingEntity entity);
-}
