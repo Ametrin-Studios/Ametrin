f90dc9df3da9b0c30940a994967942f853cec72b
Barion <kothe@botos.de>
2023-04-03 10:00:49 +0200

    fixed ItemRegistryHelper

--- src/main/java/com/ametrinstudios/ametrin/world/item/helper/ItemRegistryHelper.java
+++ src/main/java/com/ametrinstudios/ametrin/world/item/helper/ItemRegistryHelper.java
@@ -9,8 +9,8 @@ import java.util.function.Supplier;
 
 // TODO: rename -> ItemRegisterHelper
 public class ItemRegistryHelper {
-    private static Supplier<CustomBoatItem> chestBoat(CustomBoatType type) {return boat(type, BoatVariants.CHEST);}
-    private static Supplier<CustomBoatItem> boat(CustomBoatType type) {return boat(type, BoatVariants.DEFAULT);}
-    private static Supplier<CustomBoatItem> boat(CustomBoatType type, BoatVariant<?> variant) {return ()-> new CustomBoatItem(type, variant);}
+    public static Supplier<CustomBoatItem> chestBoat(CustomBoatType type) {return boat(type, BoatVariants.CHEST);}
+    public static Supplier<CustomBoatItem> boat(CustomBoatType type) {return boat(type, BoatVariants.DEFAULT);}
+    public static Supplier<CustomBoatItem> boat(CustomBoatType type, BoatVariant<?> variant) {return ()-> new CustomBoatItem(type, variant);}
 
 }
--- src/test/java/com/ametrinstudios/ametrin_test/world/TestItems.java
+++ src/test/java/com/ametrinstudios/ametrin_test/world/TestItems.java
@@ -1,6 +1,5 @@
 package com.ametrinstudios.ametrin_test.world;
 
-import com.ametrinstudios.ametrin.world.entity.boat.BoatVariants;
 import com.ametrinstudios.ametrin.world.item.CustomBoatItem;
 import com.ametrinstudios.ametrin_test.AmetrinTestMod;
 import net.minecraft.world.item.BlockItem;
@@ -10,12 +9,15 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import static com.ametrinstudios.ametrin.world.item.helper.ItemRegistryHelper.boat;
+import static com.ametrinstudios.ametrin.world.item.helper.ItemRegistryHelper.chestBoat;
+
 public class TestItems {
     public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, AmetrinTestMod.MOD_ID);
 
     public static final RegistryObject<Item> TEST_ITEM = REGISTRY.register("test_item", ()-> new BlockItem(TestBlocks.TEST_BLOCK.get(), new Item.Properties().setNoRepair().stacksTo(16).rarity(Rarity.EPIC)));
-    public static final RegistryObject<CustomBoatItem> TROLL_BOAT = REGISTRY.register("troll_boat", ()-> new CustomBoatItem(TestBoatTypes.TROLL, BoatVariants.DEFAULT));
-    public static final RegistryObject<CustomBoatItem> TROLL_CHEST_BOAT = REGISTRY.register("troll_chest_boat", ()-> new CustomBoatItem(TestBoatTypes.TROLL, BoatVariants.CHEST));
-    public static final RegistryObject<CustomBoatItem> BEECH_BOAT = REGISTRY.register("beech_boat", ()-> new CustomBoatItem(TestBoatTypes.BEECH, BoatVariants.DEFAULT));
-    public static final RegistryObject<CustomBoatItem> BEECH_CHEST_BOAT = REGISTRY.register("beech_chest_boat", ()-> new CustomBoatItem(TestBoatTypes.BEECH, BoatVariants.CHEST));
+    public static final RegistryObject<CustomBoatItem> TROLL_BOAT = REGISTRY.register("troll_boat", boat(TestBoatTypes.TROLL));
+    public static final RegistryObject<CustomBoatItem> TROLL_CHEST_BOAT = REGISTRY.register("troll_chest_boat", chestBoat(TestBoatTypes.TROLL));
+    public static final RegistryObject<CustomBoatItem> BEECH_BOAT = REGISTRY.register("beech_boat", boat(TestBoatTypes.BEECH));
+    public static final RegistryObject<CustomBoatItem> BEECH_CHEST_BOAT = REGISTRY.register("beech_chest_boat", chestBoat(TestBoatTypes.BEECH));
 }
