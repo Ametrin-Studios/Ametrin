cf484f28a1c69e98c503bbba8cfe7369473a2c35
Barion <ametrin.studios@gmail.com>
2023-08-02 16:36:14 +0200

    update

---
+++ src/test/java/com/ametrinstudios/ametrin_test/world/TestArmors.java
@@ -0,0 +1,7 @@
+package com.ametrinstudios.ametrin_test.world;
+
+import com.ametrinstudios.ametrin.util.CustomModelArmor;
+
+public class TestArmors {
+    //public CustomModelArmor TestArmor = new CustomModelArmor();
+}
