1d8d3e1a41f43ab1b749380eb86c30a1136444a7
Barion <barion+git@ik.me>
2026-04-03 10:10:10 +0200

    add FamilyBuilder to ExtendedLanguageProvider

--- Ametrin/src/main/java/com/ametrinstudios/ametrin/data/provider/ExtendedLanguageProvider.java
+++ Ametrin/src/main/java/com/ametrinstudios/ametrin/data/provider/ExtendedLanguageProvider.java
@@ -1,5 +1,6 @@
 package com.ametrinstudios.ametrin.data.provider;
 
+import net.minecraft.data.BlockFamily;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.data.LanguageProvider;
@@ -11,5 +12,64 @@ public abstract class ExtendedLanguageProvider extends LanguageProvider {
 
     protected void add(ItemLike itemLike, String name) {
         add(itemLike.asItem(), name);
+    }
+
+    public FamilyBuilder family(String name) {
+        return new FamilyBuilder(this, name);
+    }
+
+    public static final class FamilyBuilder {
+        private final ExtendedLanguageProvider provider;
+        private final String name;
+
+        public FamilyBuilder(ExtendedLanguageProvider provider, String name) {
+            this.provider = provider;
+            this.name = name;
+        }
+
+        public FamilyBuilder family(BlockFamily family) {
+            family.getVariants().forEach((variant, block) -> {
+                switch (variant) {
+                    case BUTTON -> button(block);
+                    case STAIRS -> stairs(block);
+                    case SLAB -> slab(block);
+                    case WALL -> wall(block);
+                    case FENCE, CUSTOM_FENCE -> fence(block);
+                    case FENCE_GATE, CUSTOM_FENCE_GATE -> fenceGate(block);
+                }
+            });
+
+            return this;
+        }
+
+        public FamilyBuilder button(ItemLike button) {
+            provider.add(button, name + " Button");
+            return this;
+        }
+
+        public FamilyBuilder stairs(ItemLike stairs) {
+            provider.add(stairs, name + " Stairs");
+            return this;
+        }
+
+        public FamilyBuilder slab(ItemLike slab) {
+            provider.add(slab, name + " Slab");
+            return this;
+        }
+
+        public FamilyBuilder wall(ItemLike slab) {
+            provider.add(slab, name + " Wall");
+            return this;
+        }
+
+        public FamilyBuilder fence(ItemLike slab) {
+            provider.add(slab, name + " Fence");
+            return this;
+        }
+
+        public FamilyBuilder fenceGate(ItemLike slab) {
+            provider.add(slab, name + " Fence Gate");
+            return this;
+        }
     }
 }
